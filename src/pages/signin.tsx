d0dd502ea3a264b12753072ed55c2c7faf533a6a
diskszk <ibalucky23@gmail.com>
2021-03-26 14:39:53 +0900

    fix: delete unused compoments

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,12 +1,9 @@
 import Link from 'next/link';
-import Error from '../components/organisms/Error';
 
 const SignIn: React.FC = () => {
   return (
     <div>
       <p>もどる</p>
-
-      <Error />
 
       <Link href="/">もどる</Link>
     </div>
